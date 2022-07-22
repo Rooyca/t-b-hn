273a75ca238aeb2829215a5791c0a3da925d626c
rooyca <rooyca@gmail.com>
2022-07-21 21:05:19 -0500

    Action updata

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-import os
-import requests
-
+import os, requests
 
 def main():
-    url = os.environ["INPUT_URL"]
-    user = os.environ["INPUT_USER"]
-    passw = os.environ["INPUT_PASSW"]
+    try:
+        url = os.environ["INPUT_URL"]
+    except KeyError:
+        print("::set-output name=Response::No URL provided")
+        return
 
-    response = requests.post(url, auth=(user, passw))
+    try:
+        user = os.environ["INPUT_USER"]
+        passw = os.environ["INPUT_PASSW"]
+    except KeyError:
+        user, passw = None, None
 
-    print(f"::set-output name=Status::{response.status_code}")
-    print(f"::set-output name=Response::{response.text}")
+    method = os.environ["INPUT_METHOD"]
 
+    def response(res):
+        print(f"::set-output name=Status::{res.status_code}")
+        print(f"::set-output name=Response::{res.text}")
+
+    if method == "POST":
+        res_post = requests.post(url, auth=(user, passw))
+        response(res_post)
+    elif method == "PUT":
+        resp_put = requests.put(url, auth=(user, passw))
+        response(resp_put)
+    elif method == "DELETE":
+        resp_del = requests.delete(url, auth=(user, passw))
+        response(resp_del)
+    else:
+        resp_get = requests.get(url, auth=(user, passw))
+        response(resp_get)
 
 if __name__ == "__main__":
     main()
